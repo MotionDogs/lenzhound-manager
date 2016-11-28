0fc0a3e092043c87a35cb01907719c62815b1f8b
Doug Thayer <dougathayer@gmail.com>
2016-11-27 20:36:15 -0800

    fix bg image sizing

--- src/app-root.js
+++ src/app-root.js
@@ -86,6 +86,7 @@ var Root = React.createClass({
                 position: 'absolute',
                 width: '100%',
                 height: '100%',
+                backgroundSize: '100% 100%',
             },
             innerDiv: {
                 background: 'url(content/white_texture.png)',
